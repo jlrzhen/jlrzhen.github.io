ca8ec030fd125dd4a77dd20064d94a171b3af1a3
Jeffrey <348627894@gapps.yrdsb.ca>
2021-07-03 22:21:45 -0400

    Added link to GitHub profile

--- src/components/LearnMorePage.js
+++ src/components/LearnMorePage.js
@@ -7,7 +7,8 @@ function LearnMorePage() {
                 <h1>Learn More</h1>
                 <p>This page contains all miscellaneous information
                     related to my website.</p>
-                <h2><a href="https://github.com/jlrzhen/jlrzhen.github.io">GitHub repository</a></h2>
+                <h2><a href="https://github.com/jlrzhen/jlrzhen.github.io">GitHub repository</a></h2><br/>
+                <h2><a href="https://github.com/jlrzhen">GitHub profile</a></h2><br/>
             </main>
         </div>
     );
