032bcea0167df221d6ac4e6b52d70b0b5059b0b6
Jeffrey <348627894@gapps.yrdsb.ca>
2021-07-04 22:54:48 -0400

    Added to learn more page

--- src/components/LearnMorePage.js
+++ src/components/LearnMorePage.js
@@ -7,9 +7,15 @@ function LearnMorePage() {
             <main>
                 <h1>Learn More</h1>
                 <p>This page contains all miscellaneous information
-                    related to my website.</p>
+                    related to this website and myself.</p>
                 <h2><a href="https://github.com/jlrzhen/jlrzhen.github.io">GitHub repository</a></h2><br/>
                 <h2><a href="https://github.com/jlrzhen">GitHub profile</a></h2><br/>
+                <h1>About Me</h1>
+                <p>I am currently a student planning to study Computer
+                    Engineering at the University of Waterloo. My main interests
+                    are robotics and technology. I am always looking forward
+                    to connect with, help out, and learn from like-minded people.  
+                </p>
             </main>
             </div>
         </div>
