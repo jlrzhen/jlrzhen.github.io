7742960678ae62a29d8c80940df4040966030d40
Jeffrey <348627894@gapps.yrdsb.ca>
2021-07-06 16:03:43 -0400

    Added project title

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -8,6 +8,7 @@ function ProjectsPage() {
                 <h1>Ongoing Projects</h1> 
                 <p>This page shows a collection of my 
                     completed and ongoing projects.</p>
+                <h2><a href="https://github.com/jlrzhen/name-generator">name-generator</a></h2><br/>
             </main>
             </div>    
         </div>   
