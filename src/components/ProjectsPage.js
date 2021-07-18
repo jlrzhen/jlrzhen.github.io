89090bcd653955b6d59c399edf27af977194e7a5
Jeffrey <348627894@gapps.yrdsb.ca>
2021-07-18 18:35:07 -0400

    Added description for personal website

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -10,12 +10,18 @@ function ProjectsPage() {
                     completed and ongoing projects, as well as
                     the programming languages, frameworks and
                     tools that I've used to create them.</p>
-                <h1>Ongoing</h1> 
-                <h2><a href="https://github.com/jlrzhen/jlrzhen.github.io">Personal Website</a></h2>
+                <h2 style={{textDecoration: 'none'}}>Ongoing</h2> 
+                <br/>
+                <h3><a href="https://github.com/jlrzhen/jlrzhen.github.io">Personal Website</a></h3>
                 <span> - JavaScript, React</span><br/>
-                <h2><a href="https://github.com/jlrzhen/name-generator">name-generator</a></h2>
+                <p>The purpose of this site is to show a collection 
+                    of my personal projects, while allowing me to 
+                    demonstrate concepts I've learned from front-end 
+                    web development using React.</p>
+                <h3><a href="https://github.com/jlrzhen/name-generator">name-generator</a></h3>
                 <span> - JavaScript, React</span><br/>
-                <h1>Completed</h1> 
+                <h2 style={{textDecoration: 'none'}}>Completed</h2> 
+                <br/>
             </main>
             </div>    
         </div>   
