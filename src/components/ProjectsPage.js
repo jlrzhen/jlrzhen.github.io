bbff16149bc0b8b3cff04faefb7cb7073c0787db
Jeffrey <348627894@gapps.yrdsb.ca>
2021-07-19 15:44:40 -0400

    Added description for project

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -5,6 +5,7 @@ function ProjectsPage() {
         <div className="page">
             <div className="pageContent">
             <main>
+                {/* TODO: add link to deployed projects */}
                 <h1>Projects</h1> 
                 <p>This page shows a collection of my 
                     completed and ongoing projects, as well as
@@ -17,9 +18,17 @@ function ProjectsPage() {
                 <p>The purpose of this site is to show a collection 
                     of my personal projects, while allowing me to 
                     demonstrate concepts I've learned from front-end 
-                    web development using React.</p>
+                    web development using React.
+                </p>
                 <h3><a href="https://github.com/jlrzhen/name-generator">name-generator</a></h3>
                 <span> - JavaScript, React</span><br/>
+                <p>name-generator is a web app I made that creates 
+                    usernames for users based on a specific theme. 
+                    The app is dependent on the 
+                    <span> <a href="https://www.datamuse.com/api/">Datamuse api</a> </span>
+                    and generates usernames by running queries with
+                    specific parameters.
+                </p>
                 <h2 style={{textDecoration: 'none'}}>Completed</h2> 
                 <br/>
             </main>
