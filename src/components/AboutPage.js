38769693fc1ccc8794e26744a257f71fdaf44f4e
Jeffrey <348627894@gapps.yrdsb.ca>
2021-08-29 16:20:58 -0400

    Fixed header style

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -19,13 +19,13 @@ function AboutPage() {
                 </p>
                 <br/><div className="lineDiv"/>
                 <h1>Overview of Pages</h1>
-                <ul className="aboutNav">
+                <ul>
                     <li><Link to="/home" style={linkStyle}><p className="aboutNav">My Website</p></Link></li>
                     <li><Link to="/about" style={linkStyle}><p className="aboutNav">About (You are here!)</p></Link></li>
                     <li><Link to="/projects" style={linkStyle}><p className="aboutNav">Projects</p></Link></li>
                     <li><Link to="/learnmore" style={linkStyle}><p className="aboutNav">Learn More</p></Link></li>
                 </ul> 
-                <Link to="/home" ><h2 className="aboutNav">My Website</h2></Link>
+                <Link to="/home" ><h2 className="aboutNavLink">My Website</h2></Link>
                 <p>
                     The "My Website" page is the homepage of this site.
                     This is the default page that users are redirected
@@ -35,19 +35,19 @@ function AboutPage() {
                     So far, I have implemented a clock that shows the current time 
                     using React component states and lifecycle methods. 
                 </p>
-                <Link to="/about" ><h2 className="aboutNav">About</h2></Link>
+                <Link to="/about" ><h2 className="aboutNavLink">About</h2></Link>
                 <p>
                     This is the page that you are currently viewing.
                     Here you will find an overview of all the pages 
                     on this website, as well as an explanation of the
                     purpose and features of each page.
                 </p>
-                <Link to="/projects" ><h2 className="aboutNav">Projects</h2></Link>
+                <Link to="/projects" ><h2 className="aboutNavLink">Projects</h2></Link>
                 <p>
                     The Projects page shows a collection of my 
                     completed and ongoing projects. 
                 </p>
-                <Link to="/learnmore" ><h2 className="aboutNav">Learn More</h2></Link>
+                <Link to="/learnmore" ><h2 className="aboutNavLink">Learn More</h2></Link>
                 <p>
                     This page contains all miscellaneous information
                     related to this website and myself, including a link to a 
